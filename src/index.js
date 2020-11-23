bb3833ce6367a0c74a1979a63938176ccbf2e60e
Nils Effinghausen <nils.effinghausen@gmail.com>
2020-11-23 18:52:29 +0100

    flex config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const heartbeats = require('heartbeats');
 const axios = require('axios');
 
+id = process.argv[2]
+baseURL = process.argv[3]
+
 let heart = heartbeats.createHeart(1000);
 let http = axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: baseURL
 });
 
 
 heart.createEvent(30, function(count, last) {
 
-    http.get('/12345')
+    http.get('/'+ id)
         .then(function (response) {
             console.log(response);
         })
