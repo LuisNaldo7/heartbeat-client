50bde72d5abee2451cf19f146cab1922049368fb
Nils Effinghausen <nils.effinghausen@maibornwolff.de>
2021-04-04 01:08:55 +0200

    format

--- src/index.js
+++ src/index.js
@@ -2,21 +2,19 @@ require('dotenv').config();
 const heartbeats = require('heartbeats');
 const axios = require('axios');
 
-let heart = heartbeats.createHeart(1000);
+const MILLI_SECS = 1000;
+let heart = heartbeats.createHeart(MILLI_SECS);
 let http = axios.create({
   baseURL: process.env['HEARTBEAT_SERVER_BASE_URL'],
 });
 
-heart.createEvent(
-  process.env['HEARTBEAT_CLIENT_INTERVAL'],
-  function (count, last) {
-    http
-      .get('/pulse/beat/' + process.env['HEARTBEAT_CLIENT_ID'])
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  },
-);
+heart.createEvent(process.env['HEARTBEAT_CLIENT_INTERVAL'], (count, last) => {
+  http
+    .get('/pulse/beat/' + process.env['HEARTBEAT_CLIENT_ID'])
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
