4dfb29a5fb2c39099e84d6e691ba3728069ef2a1
Nils Effinghausen <nils.effinghausen@gmail.com>
2021-04-03 16:50:40 +0200

    add prettier

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-require('dotenv').config()
+require('dotenv').config();
 const heartbeats = require('heartbeats');
 const axios = require('axios');
 
 let heart = heartbeats.createHeart(1000);
 let http = axios.create({
-    baseURL: process.env['HEARTBEAT_SERVER_BASE_URL']
+  baseURL: process.env['HEARTBEAT_SERVER_BASE_URL'],
 });
 
-heart.createEvent(process.env['HEARTBEAT_CLIENT_INTERVAL'], 
-    function(count, last) {
-        http.get('/pulse/beat/'+ process.env['HEARTBEAT_CLIENT_ID'])
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-});
+heart.createEvent(
+  process.env['HEARTBEAT_CLIENT_INTERVAL'],
+  function (count, last) {
+    http
+      .get('/pulse/beat/' + process.env['HEARTBEAT_CLIENT_ID'])
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+);
