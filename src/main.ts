34d1823afc5f0434069ae408f48501bd2b440d45
Nils Effinghausen <nils.effinghausen@maibornwolff.de>
2021-04-08 17:33:50 +0200

    introduce eslint

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
-require('dotenv').config();
-const heartbeats = require('heartbeats');
-const axios = require('axios');
+import dotenv from 'dotenv';
+import * as heartbeats from 'heartbeats';
+import axios from 'axios';
 
+dotenv.config();
 const MILLI_SECS = 1000;
-let heart = heartbeats.createHeart(MILLI_SECS);
-let http = axios.create({
+const heart = heartbeats.createHeart(MILLI_SECS);
+const http = axios.create({
   baseURL: process.env['HEARTBEAT_SERVER_BASE_URL'],
 });
 
 heart.createEvent(process.env['HEARTBEAT_CLIENT_INTERVAL'], (count: number) => {
   http
     .get('/pulse/beat/' + process.env['HEARTBEAT_CLIENT_ID'])
-    .then((response: any) => {
+    .then(() => {
       console.info('beat #' + count + ' succeeded');
     })
-    .catch((error: any) => {
+    .catch((error) => {
       console.error('beat #' + count + ' failed (' + error + ')');
     });
 });
