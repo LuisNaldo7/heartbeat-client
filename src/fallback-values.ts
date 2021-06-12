9352d58f80badcd65eeb336306bd44639205339d
Nils Effinghausen <nils.effinghausen@maibornwolff.de>
2021-06-12 16:02:46 +0200

    add fallback values

---
+++ src/fallback-values.ts
@@ -0,0 +1,5 @@
+export const fallback = {
+  HEARTBEAT_SERVER_BASE_URL: 'http://localhost:3000',
+  HEARTBEAT_CLIENT_INTERVAL: 30,
+  HEARTBEAT_CLIENT_ID: 'undefined',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
 import dotenv from 'dotenv';
 import * as heartbeats from 'heartbeats';
 import axios from 'axios';
+import { fallback } from './fallback-values';
 
 dotenv.config();
 const MILLI_SECS = 1000;
 const heart = heartbeats.createHeart(MILLI_SECS);
 const http = axios.create({
-  baseURL: process.env['HEARTBEAT_SERVER_BASE_URL'],
+  baseURL:
+    process.env.HEARTBEAT_SERVER_BASE_URL || fallback.HEARTBEAT_SERVER_BASE_URL,
 });
 
-heart.createEvent(process.env['HEARTBEAT_CLIENT_INTERVAL'], (count: number) => {
-  http
-    .get('/pulse/beat/' + process.env['HEARTBEAT_CLIENT_ID'])
-    .then(() => {
-      console.info('beat #' + count + ' succeeded');
-    })
-    .catch((error) => {
-      console.error('beat #' + count + ' failed (' + error + ')');
-    });
-});
+heart.createEvent(
+  process.env.HEARTBEAT_CLIENT_INTERVAL || fallback.HEARTBEAT_CLIENT_INTERVAL,
+  (count: number) => {
+    http
+      .get(
+        '/pulse/beat/' + process.env.HEARTBEAT_CLIENT_ID ||
+          fallback.HEARTBEAT_CLIENT_ID,
+      )
+      .then(() => {
+        console.info('beat #' + count + ' succeeded');
+      })
+      .catch((error) => {
+        console.error('beat #' + count + ' failed (' + error + ')');
+      });
+  },
+);
