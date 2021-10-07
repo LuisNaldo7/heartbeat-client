a7cc360aa7005062b2fac468d9e95e4fb4b465e2
Nils Effinghausen <nils.effinghausen@maibornwolff.de>
2021-10-07 16:02:13 +0200

    add timestamp

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import * as heartbeats from 'heartbeats';
 import axios from 'axios';
 import { fallback } from './fallback-values';
+import date from 'date-and-time';
 
 dotenv.config();
 const MILLI_SECS = 1000;
@@ -20,11 +21,21 @@ heart.createEvent(
           process.env.HEARTBEAT_CLIENT_ID || fallback.HEARTBEAT_CLIENT_ID,
         type: 'BEAT',
       })
-      .then((result) => {
-        console.info('beat #' + count + ': successful');
+      .then(() => {
+        console.info(getTimestamp() + ' - beat #' + count + ': successful');
       })
       .catch((error) => {
-        console.error('beat #' + count + ': failed - ' + error);
+        console.error(
+          getTimestamp() + ' - beat #' + count + ': failed - ' + error,
+        );
       });
   },
 );
+
+function getTimestamp() {
+  return date.format(
+    new Date(),
+    process.env.HEARTBEAT_CLIENT_DATE_FORMAT ||
+      fallback.HEARTBEAT_CLIENT_DATE_FORMAT,
+  );
+}
